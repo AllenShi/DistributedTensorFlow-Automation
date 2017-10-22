05804c9f75e7ce5dbd5f0345cb798ac8494c55ff
Allen Shi <jinliangshi@gmail.com>
2017-10-21 18:34:39 -0700

    clean some wording

--- start-tf-worker-test.sh
+++ start-tf-worker-test.sh
@@ -5,7 +5,7 @@ export DL_WORK_SPACES=${DL_WORK_HOME}/projects
 export DL_TF_MODEL_DIR=${DL_WORK_SPACES}/Google/tensorflow/models/research
 export PYTHONPATH=$PYTHONPATH:${DL_TF_MODEL_DIR}:${DL_TF_MODEL_DIR}/slim
 
-export TF_CONFIG="{\"cluster\": {\"master\": [\"monitorsvr1.fyre.ibm.com:2220\"], \"ps\": [\"monitorsvr1.fyre.ibm.com:2221\"], \"worker\": [\"monitorsvr1.fyre.ibm.com:2222\"]}, \"task\": {\"type\": \"worker\", \"index\": 0}}"
+export TF_CONFIG="{\"cluster\": {\"master\": [\"monitorsvr1.fyre.io:2220\"], \"ps\": [\"monitorsvr1.fyre.io:2221\"], \"worker\": [\"monitorsvr1.fyre.io:2222\"]}, \"task\": {\"type\": \"worker\", \"index\": 0}}"
 
 source ${DL_WORK_HOME}/virtualenv/python3/bin/activate
 
--- start-tf-worker0.sh
+++ start-tf-worker0.sh
@@ -5,7 +5,7 @@ export DL_WORK_SPACES=${DL_WORK_HOME}/projects
 export DL_TF_MODEL_DIR=${DL_WORK_SPACES}/Google/tensorflow/models/research
 export PYTHONPATH=$PYTHONPATH:${DL_TF_MODEL_DIR}:${DL_TF_MODEL_DIR}/slim
 
-export TF_CONFIG="{\"cluster\": {\"master\": [\"oxr-dev1.fyre.ibm.com:2220\"], \"ps\": [\"oxr-dev2.fyre.ibm.com:2221\"], \"worker\": [\"oxr-dev2.fyre.ibm.com:2222\", \"oxr-dev3.fyre.ibm.com:2222\", \"oxr-dev4.fyre.ibm.com:2222\", \"oxr-dev5.fyre.ibm.com:2222\", \"oxr-dev6.fyre.ibm.com:2222\"]}, \"task\": {\"type\": \"worker\", \"index\": 0}}"
+export TF_CONFIG="{\"cluster\": {\"master\": [\"oxr-dev1.fyre.io:2220\"], \"ps\": [\"oxr-dev2.fyre.io:2221\"], \"worker\": [\"oxr-dev2.fyre.io:2222\", \"oxr-dev3.fyre.io:2222\", \"oxr-dev4.fyre.io:2222\", \"oxr-dev5.fyre.io:2222\", \"oxr-dev6.fyre.io:2222\"]}, \"task\": {\"type\": \"worker\", \"index\": 0}}"
 
 source ${DL_WORK_HOME}/virtualenv/python3/bin/activate
 
